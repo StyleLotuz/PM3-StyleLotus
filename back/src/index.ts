540b40d26928beeeaa98fc7178ea68bdb29db7c6
James Tellez <106494680+StyleLotus@users.noreply.github.com>
2024-05-10 04:13:18 -0500

    HW9 Done
    We should work on the styles

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,14 +1,12 @@
 import server from "./server";
 import { PORT, HOST, PROTO } from "./config/envs";
-import "reflect-metadata"
+import "reflect-metadata";
 import { AppDataSource } from "./config/data-source";
 
-AppDataSource.initialize()
-.then(res =>{
+AppDataSource.initialize().then((res) => {
 	console.log("Se ha realizado una conexión con el servidor");
-	console.log(HOST)
-	
-	server.listen(PORT, HOST, () => {
+
+	server.listen(PORT, () => {
 		console.log(`Server listening on ${PROTO}://${HOST}:${PORT}`);
 	});
-})
+});
--- front/src/views/MyAppointments/MyAppointments.jsx
+++ front/src/views/MyAppointments/MyAppointments.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
-import { appointments } from '../../helpers/myAppointments'
-import Appointment from '../../components/secundary/Appointment/Appointment'
-import styles from "./myAppointments.module.css"
+import React, { useEffect, useState } from "react";
+import Appointment from "../../components/secundary/Appointment/Appointment";
+import styles from "./myAppointments.module.css";
+import axios from "axios";
 
-export default function () {
-    const [citas, setCitas] = useState(appointments)
+export default function MyAppointments() {
+	const [citas, setCitas] = useState([]);
 
-    return (
-        <div className={styles.container}>
-            {citas.map((cita) => (
-                <Appointment key={cita.id}
-                    date={cita.date}
-                    time={cita.time}
-                    status={cita.status}
-                />
-            ))}
-        </div>
-    )
+	useEffect(() => {
+		axios
+			.get("http://localhost:3000/appointments")
+			.then((response) => response.data)
+			.then((data) => setCitas(data))
+			.catch((error) => console.error("Error al obtener los datos de la base de datos", error));
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			{citas.map((cita) => (
+				<Appointment key={cita.id} date={cita.date} time={cita.time} status={cita.status} />
+			))}
+		</div>
+	);
 }
